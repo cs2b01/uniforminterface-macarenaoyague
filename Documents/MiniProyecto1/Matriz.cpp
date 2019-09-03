e25478bc40e293d7bceefc63508903f0b378cb0a
Macarena <macarena.oyague@utec.edu.pe>
2019-09-02 21:09:25 -0500

    Update

---
+++ Documents/MiniProyecto1/Matriz.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Macarena Oyague on 2019-09-02.
+//
+
+#include "Matriz.h"
---
+++ Documents/MiniProyecto1/cmake-build-debug/CMakeFiles/MiniProyecto1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MiniProyecto1.dir/main.cpp.o"
+  "CMakeFiles/MiniProyecto1.dir/Matriz.cpp.o"
+  "MiniProyecto1.pdb"
+  "MiniProyecto1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MiniProyecto1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
